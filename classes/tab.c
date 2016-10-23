a0fb89a20c2e640070a28051d92e0e573e015742
Joe Watkins <krakjoe@php.net>
2016-10-23 06:31:12 +0100

    correct arginfo

--- classes/tab.c
+++ classes/tab.c
@@ -139,7 +139,7 @@ PHP_METHOD(Tab, setMargin)
 	uiTabSetMargined(tab->t, (int) page, (int) margin);
 } /* }}} */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(php_ui_tab_has_margin_info, 0, 0, _IS_BOOL, NULL, 1)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(php_ui_tab_has_margin_info, 0, 1, _IS_BOOL, NULL, 0)
 	ZEND_ARG_TYPE_INFO(0, page, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
