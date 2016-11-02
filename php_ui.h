7ef2ada0d18e6e96afa6ce1bedf6383bd13abf21
Joe Watkins <krakjoe@php.net>
2016-11-02 06:12:00 +0000

    bump after release

--- php_ui.h
+++ php_ui.h
@@ -24,7 +24,7 @@
 extern zend_module_entry ui_module_entry;
 #define phpext_ui_ptr &ui_module_entry
 
-#define PHP_UI_VERSION "2.0.0"
+#define PHP_UI_VERSION "2.0.1dev"
 
 #ifdef PHP_WIN32
 #	define PHP_UI_API __declspec(dllexport)
